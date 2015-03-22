136d09330133237e86b2c1a64c078092f5886016
Docker <Docker@jjj.local>
2015-03-22 12:19:34 -0400

    added docker file apache-config and sample php page

---
+++ www/index.php
@@ -0,0 +1,26 @@
+
+
+<?php
+$servername = "172.17.0.32";
+$username = "root";
+$password = "password";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO Persons (Name)
+VALUES ('John')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
